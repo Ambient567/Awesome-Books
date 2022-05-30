aa523a9738f768a3dc48aae6dbe43853ac37b98c
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-05-30 14:11:40 +0000

    Generate book section dynamically

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-[
-    {
-        title: 'Title',
-        autor: 'Autour' 
-    },
+const books = [
+  {
+    title: 'Ghana Boy',
+    autor: 'Ashe',
+  },
 
-    {
-        title: 'Title',
-        autor: 'Autour' 
-    },
-]
+  {
+    title: 'Once upon a time',
+    autor: 'Killy',
+  },
+];
+
+const bookSection = document.querySelector('#books-section');
+const addBookButton = document.querySelector('#add-book-button');
+
+function addBook() {
+  const addTitle = document.querySelector('.add-title');
+  const addAuthor = document.querySelector('.add-author');
+
+  books.push({
+    title: addTitle.value,
+    author: addAuthor.value,
+  });
+
+  console.log(books);
+}
+
+addBookButton.addEventListener('click', addBook);
+
+books.forEach((book) => {
+  bookSection.innerHTML += `<h4>${book.title}</h4> 
+    <h4>${book.autor}</h4>
+    <button>Remove</button>
+    <hr>`;
+});
