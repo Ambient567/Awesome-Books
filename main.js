ab8765ef8f9a6b5a4ea5cde123a25ffbffcc4dce
Ashe546 <asfyd42@gmail.com>
2022-05-30 15:00:59 -0300

    Added the functionality to the remove button

--- main.js
+++ main.js
@@ -5,23 +5,51 @@ function addBook() {
   const addTitle = document.querySelector('.add-title');
   const addAuthor = document.querySelector('.add-author');
   
-  let bookArrays = [];
+  let bookArrays = JSON.parse(localStorage.getItem('session')) || [];
 
-  const bookObjects ={
-    title: addTitle.value,
-    autor: addAuthor.value,
+  if(addTitle.value = !null && addAuthor.value){
+    const bookObjects = {
+      id: bookArrays.length,
+      title: addTitle.value,
+      autor: addAuthor.value,
+    }
+    bookArrays.push(bookObjects);  
+    localStorage.setItem('session', JSON.stringify(bookArrays));
+  }else{
+    alert('no input detected')
   }
-  bookArrays.push(bookObjects);  
-  localStorage.setItem('session', JSON.stringify(bookArrays));
-  console.log(bookArrays)
-
-  bookArrays.forEach((bookArray) => {
-    bookSection.innerHTML += `<h4>${bookArray.title}</h4> 
-      <h4>${bookArray.autor}</h4>
-      <button>Remove</button>
-      <hr>`;
-      console.log(session.title)
-  });
 }
 
+function retrieveFormData() {
+  const session = JSON.parse(localStorage.getItem('session')) || [];
+  session.forEach((bookArray) => {
+    if (session){
+    bookSection.innerHTML += `<h4>${bookArray.title}</h4> 
+      <h4>${bookArray.autor}</h4>
+      <button value = '${bookArray.id}' class = 'remove'>Remove</button>
+      <hr>`;
+  }
+})
+};
+
+
+function removeBook() {
+  let bookRemove = JSON.parse(localStorage.getItem('session'));
+  let x = removeButton.innerHTML
+  console.log(x)
+  localStorage.setItem('session', JSON.stringify(bookRemove));
+  console.log(bookRemove)
+
+}
+
+
+retrieveFormData();
 addBookButton.addEventListener('click', addBook);
+
+document.querySelectorAll('.remove').forEach((n) => n.addEventListener('click', () => {
+  let bookRemove = JSON.parse(localStorage.getItem('session'));
+  console.log(n)
+  //console.log(n.indexOf())
+  bookRemove.splice(n, 1)
+  localStorage.setItem('session', JSON.stringify(bookRemove));
+}));
