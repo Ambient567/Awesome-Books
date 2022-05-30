d42d97b2fbda9676de771017a37e6be332b93d12
Ashe546 <asfyd42@gmail.com>
2022-05-30 11:43:53 -0300

    Add

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
-const books = [
-  {
-    title: 'Ghana Boy',
-    autor: 'Ashe',
-  },
-
-  {
-    title: 'Once upon a time',
-    autor: 'Killy',
-  },
-];
-
 const bookSection = document.querySelector('#books-section');
 const addBookButton = document.querySelector('#add-book-button');
 
 function addBook() {
   const addTitle = document.querySelector('.add-title');
   const addAuthor = document.querySelector('.add-author');
+  
+  let bookArrays = [];
 
-  books.push({
+  const bookObjects ={
     title: addTitle.value,
-    author: addAuthor.value,
-  });
+    autor: addAuthor.value,
+  }
+  bookArrays.push(bookObjects);  
+  localStorage.setItem('session', JSON.stringify(bookArrays));
+  console.log(bookArrays)
 
-  console.log(books);
+  bookArrays.forEach((bookArray) => {
+    bookSection.innerHTML += `<h4>${bookArray.title}</h4> 
+      <h4>${bookArray.autor}</h4>
+      <button>Remove</button>
+      <hr>`;
+      console.log(session.title)
+  });
 }
 
 addBookButton.addEventListener('click', addBook);
-
-books.forEach((book) => {
-  bookSection.innerHTML += `<h4>${book.title}</h4> 
-    <h4>${book.autor}</h4>
-    <button>Remove</button>
-    <hr>`;
-});
