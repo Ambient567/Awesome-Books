ac2118ea1481478c0d27c7cded8bab71f10e3464
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-05-30 17:58:46 +0000

    Fix linter errors

--- main.js
+++ main.js
@@ -4,23 +4,22 @@ const addBookButton = document.querySelector('#add-book-button');
 function addBook() {
   const addTitle = document.querySelector('.add-title');
   const addAuthor = document.querySelector('.add-author');
-  
-  let bookArrays = [];
+  const bookArrays = [];
 
-  const bookObjects ={
+  const bookObjects = {
     title: addTitle.value,
     autor: addAuthor.value,
-  }
-  bookArrays.push(bookObjects);  
-  localStorage.setItem('session', JSON.stringify(bookArrays));
-  console.log(bookArrays)
-
+  };
+  bookArrays.push(bookObjects);
+  localStorage.setItem('bookArrays', JSON.stringify(bookArrays));
+  console.log(bookArrays);
+  
   bookArrays.forEach((bookArray) => {
     bookSection.innerHTML += `<h4>${bookArray.title}</h4> 
       <h4>${bookArray.autor}</h4>
       <button>Remove</button>
       <hr>`;
-      console.log(session.title)
+    console.log(bookArray.title);
   });
 }
 
