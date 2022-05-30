df7029ccb3284442ceb40fe8ea81b4f7c7c491fd
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-05-30 18:48:58 +0000

    ADd delete book functionality

--- main.js
+++ main.js
@@ -1,55 +1,55 @@
 const bookSection = document.querySelector('#books-section');
 const addBookButton = document.querySelector('#add-book-button');
+retrieveFormData();
 
 function addBook() {
   const addTitle = document.querySelector('.add-title');
   const addAuthor = document.querySelector('.add-author');
-  
-  let bookArrays = JSON.parse(localStorage.getItem('session')) || [];
 
-  if(addTitle.value = !null && addAuthor.value){
+  const bookArrays = JSON.parse(localStorage.getItem('session')) || [];
+
+  if (addTitle.value !== '' && addAuthor.value !== '') {
     const bookObjects = {
-      id: bookArrays.length,
       title: addTitle.value,
       autor: addAuthor.value,
-    }
-    bookArrays.push(bookObjects);  
+    };
+
+    bookArrays.push(bookObjects);
     localStorage.setItem('session', JSON.stringify(bookArrays));
-  }else{
-    alert('no input detected')
+    retrieveFormData();
+  } else {
+    alert('no input detected');
   }
 }
 
 function retrieveFormData() {
   const session = JSON.parse(localStorage.getItem('session')) || [];
   session.forEach((bookArray) => {
-    if (session){
-    bookSection.innerHTML += `<h4>${bookArray.title}</h4> 
+    if (session) {
+      bookSection.innerHTML += `<h4>${bookArray.title}</h4> 
       <h4>${bookArray.autor}</h4>
-      <button value = '${bookArray.id}' class = 'remove'>Remove</button>
+      <button class = 'remove'>Remove</button>
       <hr>`;
-  }
-})
-};
-
-
-function removeBook() {
-  let bookRemove = JSON.parse(localStorage.getItem('session'));
-  let x = removeButton.innerHTML
-  console.log(x)
-  localStorage.setItem('session', JSON.stringify(bookRemove));
-  console.log(bookRemove)
-
+    }
+  });
 }
 
+// function removeBook() {
+//   let bookRemove = JSON.parse(localStorage.getItem('session'));
+//   let x = removeButton.innerHTML
+//   console.log(x)
+//   localStorage.setItem('session', JSON.stringify(bookRemove));
+//   console.log(bookRemove)
 
-retrieveFormData();
+// }
+
 addBookButton.addEventListener('click', addBook);
 
 document.querySelectorAll('.remove').forEach((n) => n.addEventListener('click', () => {
-  let bookRemove = JSON.parse(localStorage.getItem('session'));
-  console.log(n)
-  //console.log(n.indexOf())
-  bookRemove.splice(n, 1)
+  const bookRemove = JSON.parse(localStorage.getItem('session'));
+  console.log(n);
+  // console.log(n.indexOf())
+  bookRemove.splice(n, 1);
+  bookSection.innerHTML -= '';
   localStorage.setItem('session', JSON.stringify(bookRemove));
 }));
