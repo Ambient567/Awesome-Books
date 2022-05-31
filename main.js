b8930573190d5d1a599f66bad4b3114deb62cdaf
Ashe546 <asfyd42@gmail.com>
2022-05-31 13:43:01 -0300

    Fixed add and remove button error

--- main.js
+++ main.js
@@ -1,55 +1,53 @@
 const bookSection = document.querySelector('#books-section');
 const addBookButton = document.querySelector('#add-book-button');
-retrieveFormData();
+const addTitle = document.querySelector('.add-title');
+const addAuthor = document.querySelector('.add-author');
+
+const bookArrays = JSON.parse(localStorage.getItem('session')) || [];
+
+function retrieveFormData() {
+    
+  bookArrays.forEach((bookArray, i) => {
+      bookSection.innerHTML += `<h4>${bookArray.title}</h4> 
+      <h4>${bookArray.autor}</h4>
+      <button class = 'remove' onclick="removeBook(${i})">Remove</button>
+      <hr>`;
+  })
+};
+
 
 function addBook() {
-  const addTitle = document.querySelector('.add-title');
-  const addAuthor = document.querySelector('.add-author');
-
-  const bookArrays = JSON.parse(localStorage.getItem('session')) || [];
 
   if (addTitle.value !== '' && addAuthor.value !== '') {
     const bookObjects = {
       title: addTitle.value,
       autor: addAuthor.value,
     };
-
     bookArrays.push(bookObjects);
-    localStorage.setItem('session', JSON.stringify(bookArrays));
     retrieveFormData();
+    localStorage.setItem('session', JSON.stringify(bookArrays));
+    window.location.reload()
+    
   } else {
     alert('no input detected');
   }
 }
 
-function retrieveFormData() {
-  const session = JSON.parse(localStorage.getItem('session')) || [];
-  session.forEach((bookArray) => {
-    if (session) {
-      bookSection.innerHTML += `<h4>${bookArray.title}</h4> 
-      <h4>${bookArray.autor}</h4>
-      <button class = 'remove'>Remove</button>
-      <hr>`;
-    }
-  });
-}
+window.onload = () => {
+  if (localStorage.getItem('session')) {
+    session = JSON.parse(localStorage.getItem('session'));
+  }
+  retrieveFormData();
+};
 
-// function removeBook() {
-//   let bookRemove = JSON.parse(localStorage.getItem('session'));
-//   let x = removeButton.innerHTML
-//   console.log(x)
-//   localStorage.setItem('session', JSON.stringify(bookRemove));
-//   console.log(bookRemove)
-
-// }
 
 addBookButton.addEventListener('click', addBook);
 
-document.querySelectorAll('.remove').forEach((n) => n.addEventListener('click', () => {
-  const bookRemove = JSON.parse(localStorage.getItem('session'));
-  console.log(n);
-  // console.log(n.indexOf())
-  bookRemove.splice(n, 1);
-  bookSection.innerHTML -= '';
-  localStorage.setItem('session', JSON.stringify(bookRemove));
-}));
+function removeBook(index) {
+  bookArrays.splice(index, 1);
+  localStorage.setItem('session', JSON.stringify(bookArrays));
+  retrieveFormData();
+  window.location.reload()
+}
+
+
