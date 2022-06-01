05a4bc944135019daf482dbc906a0224cc9907ca
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-06-01 02:03:54 +0000

    Add functionality to add and delete from local storage

--- main.js
+++ main.js
@@ -11,12 +11,63 @@ class Book {
   }
 }
 
+// Local Storage
+// Retrive books from Local storage
+function getBooks() {
+  let bookCollection;
+  if (localStorage.getItem('bookApp') === null) {
+    bookCollection = [];
+  } else {
+    bookCollection = JSON.parse(localStorage.getItem('bookApp'));
+  }
+  return bookCollection;
+}
+
+// Function to Add book to Local Storage
+function addBookToLocalStorage(book) {
+  const bookCollection = getBooks();
+  bookCollection.push(book);
+  localStorage.setItem('bookApp', JSON.stringify(bookCollection));
+}
+
+// Function to Remove Book from Local Storage
+function removeBook(id) {
+  const bookCollection = getBooks();
+  bookCollection.forEach((book, index) => {
+    if (book.id === id) {
+      bookCollection.splice(index, 1);
+    }
+    localStorage.setItem('bookApp', JSON.stringify(bookCollection));
+  });
+}
+
 // Updating the UI: Function to create a new book list in UI
 function addBookToList(book) {
   bookContainer.innerHTML += `
                             
-    <li>"${book.title}" by ${book.author} <button type="button" class="remove">Remove</button> <span hidden>${book.id}</span></li>`;
+    <li class="book">"${book.title}" by ${book.author} <button type="button" class="remove">Remove</button> <span hidden>${book.id}</span></li>`;
 }
+
+// Function to show Books in UI
+function displayBooks() {
+  const books = getBooks();
+  books.forEach((book) => {
+    addBookToList(book);
+  });
+}
+
+// Event for Displaying Books in UI
+document.addEventListener('DOMContentLoaded', displayBooks);
+
+// Add event to Book list buttons
+bookContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove')) {
+    const currentBook = e.target.closest('.book');
+    currentBook.remove();
+    const id = currentBook.querySelector('span').textContent;
+    removeBook(Number(id));
+  }
+});
 
 // Event for submiting a new book to the list
 form.addEventListener('submit', (e) => {
@@ -28,6 +79,7 @@ form.addEventListener('submit', (e) => {
   if (titleInput.value.length > 0 && authorInput.value.length > 0) {
     const newBook = new Book(titleInput.value, authorInput.value);
     addBookToList(newBook);
+    addBookToLocalStorage(newBook);
     titleInput.value = '';
     authorInput.value = '';
     titleInput.focus();
